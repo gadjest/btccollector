4b3c10cfcce6b8e5e8b0da86ddf140a79f80e583
gadjest <46781530+gadjest@users.noreply.github.com>
2019-04-05 06:39:20 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//Masukkan PHPSESSID
+$PHPSESSID = 'qhjv11mv04h9ls8vocr820q091';
+//Masukkan GA
+$_ga = 'GA1.2.1851426583.1554379777';
+//Masukkan GID
+$_gid = 'GA1.2.1645366372.1554379777':
+?>
